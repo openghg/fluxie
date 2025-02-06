4e8c9aae0c4e6655977d38f24a64bf3d89e52bd3
Alexandre Danjou <bq24992@bristol.ac.uk>
2025-02-06 12:14:24 +0000

    Formatting on update_list_params

--- fluxy/plots/utils.py
+++ fluxy/plots/utils.py
@@ -12,9 +12,8 @@ from fluxy.io import load_countries_shape
 
 logger = logging.getLogger(__name__)
 
-def update_list_params(params_to_check: list | None,
-                       expected_size: int
-                       )-> list:
+
+def update_list_params(params_to_check: list, expected_size: int) -> list:
     """
     Check if parameters are list of the expected lenght. If they are not list, convert them to list (except is it is None), raise an erro if it is a list but not of the expected size.
     Args:
@@ -26,14 +25,14 @@ def update_list_params(params_to_check: list | None,
     updated_params = list()
     for param in params_to_check:
         if param is None:
-            updated_params.append([False]*expected_size)
-        elif type(param) is list :
+            updated_params.append([False] * expected_size)
+        elif type(param) is list:
             if len(param) == expected_size:
                 updated_params.append(param)
             else:
                 raise ValueError(f'{param} must be a boolean or a list of booleans of the same length as models.')        
         else:
-            updated_params.append([param]*expected_size)
+            updated_params.append([param] * expected_size)
     return updated_params
 
 def add_colorbar(fig, ax, mappable, cmap, extend, label):
