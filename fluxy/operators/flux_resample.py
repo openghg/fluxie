Fix bugs from PR #14 - correct calc_rolling_mean to add first and last point of previous   dataset into the new one - correct the calculation of the resampled uncertainty  when assuming no   correlation - correct bug in x-axis limits in plot_country_flux

fluxy/operators/flux_resample.py
@@ -53,9 +53,9 @@ def calculate_resampled_uncertainty(
             )  # count the number of sample in each period
 
             lower = np.sqrt(((ds_all_original[m][f"{v}_lower"] - ds_all_original[m][v]) ** 2)
-                            .resample(time=rtime[i]).mean(dim="time"))
+                            .resample(time=rtime[i]).sum(dim="time")) / n_periods
             upper = np.sqrt(((ds_all_original[m][f"{v}_upper"] - ds_all_original[m][v]) ** 2)
-                            .resample(time=rtime[i]).mean(dim="time"))
+                            .resample(time=rtime[i]).sum(dim="time")) / n_periods
 
             ds_all_resampled[m][f"{v}_lower"] = ds_all_resampled[m][v] - lower
             ds_all_resampled[m][f"{v}_upper"] = ds_all_resampled[m][v] + upper