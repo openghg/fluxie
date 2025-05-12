d432991183b201071272151b10d185f4d9b3855e
coli <lionel.constantin@empa.ch>
2025-05-12 15:00:41 +0200

    adding a 4rth color

--- fluxy/config.py
+++ fluxy/config.py
@@ -8,6 +8,7 @@ color_palette = {
     0: [["blue", "dodgerblue"], ["dodgerblue", "skyblue"], ["deepskyblue", "cyan"]],
     1: [["purple", "mediumpurple"], ["deeppink", "pink"], ["darkorange", "red"]],
     2: [["darkgreen", "green"], ["limegreen", "palegreen"], ["olive", "lightgreen"]],
+    3: [["darkorange", "orange"], ["gold", "khaki"], ["yellow", "lightyellow"]],
 }
 
 # population from 2018 to 2023 (at Jan 1 each year)
