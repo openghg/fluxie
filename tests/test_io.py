c39568d2f5764e24a59ddb1de5c9876d1a1a6ce0
coli <lionel.constantin@empa.ch>
2025-05-14 09:24:42 +0200

    cleaned tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,72 +2,37 @@ from pathlib import Path
 import fluxy
 from fluxy.io import read_config_files, read_model_output, read_flux_total_fgases
 from fluxy.operators.select import slice_flux, slice_mf
+from fluxy.test_utils import data_dir
+from fluxy.test_utils.models import test_models
+import pytest
 
 
-data_dir = Path(fluxy.__path__[0]).parent / "data" / "tests"
-
-
-def test_read_flux():
-    # This test fails sometimes when runned wil all the other tests
+@pytest.mark.parametrize("model", test_models)
+def test_read_flux(model):
+    # This test fails sometimes when runned with all the other tests
     # Because of a xarray cache problem
     config_data = read_config_files()
 
     species = "hfc134a"  # options for individual species, or 'all_hfc' or 'all_pfc'
-    models = [
-        "InTEM_NAME_EUROPE_EDGAR_std",
-        "ELRIS_NAME_EUROPE_EDGAR_std",
-        "RHIME_NAME_EUROPE_EDGAR_std",
-    ]
-    period = "yearly"
-    country_flux_units_print = "Gg yr-1"
-    start_date = "2018-01-01"  # inclusive. Option to set as list of dates, e.g. ['2018-01-01','2019-01-01'] which is required for total fgases if one model is missing obs for a year
-    end_date = "2024-01-01"  # not inclusive. Option to set as list of dates, e.g. ['2023-01-01','2022-01-01'] which is required for total fgases if one model is missing obs for a year
 
-    ds_all_flux_scaled = {}
+    period = "yearly"
 
     ds_all_flux = read_model_output(
-        data_dir, "flux", species, models, config_data, period=period
+        data_dir, "flux", species, [model], config_data, period=period
     )
 
-    for m in models:
-        ds_all_flux_scaled[m] = slice_flux(
-            {m: ds_all_flux[m]},
-            config_data,
-            start_date,
-            end_date,
-            species=species,
-            country_flux_units_print=country_flux_units_print,
-        )[m]
 
-
-def test_read_mf():
+@pytest.mark.parametrize("model", test_models)
+def test_read_mf(model):
     # This test fails sometimes when runned wil all the other tests
     # Because of a xarray cache problem
     config_data = read_config_files()
 
     species = "hfc134a"
-    site = "MHD"
-    models = ["InTEM_NAME_EDGAR_std", "ELRIS_NAME_EDGAR_std", "RHIME_NAME_EDGAR_std"]
     period = "yearly"  # use to override standard inversion periods, must be a list the same length as models, e.g. ['monthly','yearly']
-    mf_units_print = "ppt"
-    start_date = "2018-01-01"  # inclusive. Option to set as list of dates, e.g. ['2018-01-01','2019-01-01'] which is required for total fgases if one model is missing obs for a year
-    end_date = "2024-01-01"  # not inclusive. Option to set as list of dates, e.g. ['2023-01-01','2022-01-01'] which is required for total fgases if one model is missing obs for a year
-    baseline_site = (
-        None  #'MHD', 'JFJ' or 'CMN'. If None, does not mask by baseline time
-    )
 
     ds_all_mf = read_model_output(
-        data_dir, "concentration", species, models, config_data, period=period
-    )
-
-    ds_all_mf_sliced = slice_mf(
-        ds_all_mf.copy(),
-        start_date,
-        end_date,
-        site,
-        baseline_site=baseline_site,
-        data_dir=data_dir,
-        mf_units_print=mf_units_print,
+        data_dir, "concentration", species, [model], config_data, period=period
     )
 
 
@@ -80,4 +45,6 @@ def test_read_config_default():
 def test_read_empty_configs():
     fake_dir = Path("/nowhere/is/this")
     config_data = read_config_files(fake_dir)
-    assert config_data == {}, "Config data should be an empty dictionary when no config files are found"
+    assert (
+        config_data == {}
+    ), "Config data should be an empty dictionary when no config files are found"
---
+++ tests/test_operators.py
@@ -0,0 +1,50 @@
+
+
+
+from fluxy.operators.select import slice_flux, slice_mf
+from fluxy.test_utils import data_dir 
+from fluxy.test_utils.models import get_loaded_models, test_models
+from fluxy.io import read_config_files, read_model_output
+from fluxy.operators.mf import compute_mf_difference
+import pytest
+import itertools
+
+
+
+ds_all_mf = get_loaded_models("concentration")
+ds_all_flux = get_loaded_models("flux")
+
+# Test the difference between all available models
+@pytest.mark.parametrize("model_pair", list(itertools.combinations(test_models, 2)))
+def test_mf_difference(model_pair):
+
+    ds_diff = compute_mf_difference(ds_all_mf, model_pair)
+
+
+@pytest.mark.parametrize("model", test_models)
+def test_slice_flux(model):
+
+    country_flux_units_print = "Gg yr-1"
+    start_date = "2018-01-01" 
+    end_date = "2024-01-01"  
+
+    dss_flux_scaled = slice_flux(
+        {model: ds_all_flux[model]},
+        start_date=start_date,
+        end_date=end_date,
+        country_flux_units_print=country_flux_units_print,
+    )
+    assert model in dss_flux_scaled
+
+@pytest.mark.parametrize("model", test_models)
+def test_slice_mf(model):
+
+    start_date = "2018-01-01" 
+    end_date = "2024-01-01"  
+
+    dss_mf_scaled = slice_mf(
+        {model: ds_all_mf[model]},
+        start_date,
+        end_date,
+    )
+    assert model in dss_mf_scaled
