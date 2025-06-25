fc686a0b084db54fcdb241df77f061178d7722b4
Alexandre Danjou <alexandre.danjou.pro@gmail.com>
2025-06-25 16:35:48 +0100

    Addition to update to python version change in github

--- fluxy/plots/flux_timeseries.py
+++ fluxy/plots/flux_timeseries.py
@@ -383,9 +383,9 @@ def plot_country_flux(
             ncol = len(ds_to_plot) + 1 if annex_mode else 2
             leg = ax.legend(ncol=ncol, borderpad=0.4, columnspacing=1.0)
 
-            # handle_name = "legend_handles" if float(matplotlib_version[:3]) > 3.7 else "legendHandles"
-            # for l in leg.__getattribute__(handle_name)[: (-1 if plot_inventory else None)]:
-            for l in leg.legendHandles[: (-1 if plot_inventory else None)]:
+            handle_name = "legend_handles" if float(matplotlib_version[:3]) > 3.7 else "legendHandles"
+            for l in leg.__getattribute__(handle_name)[: (-1 if plot_inventory else None)]:
+            # for l in leg.legendHandles[: (-1 if plot_inventory else None)]:
                 l.set_linewidth(3.0)
 
         # set title
