dd5215d3caa7b821342fd44b41d02b312c662a4a
Lionel42 <lionel.constantin@empa.ch>
2025-10-06 17:33:55 +0200

    update stack plot

--- fluxy/plots/ec_flux/sectorial_stack.py
+++ fluxy/plots/ec_flux/sectorial_stack.py
@@ -8,12 +8,11 @@ from fluxy.plots.utils import stack_plot
 WIND_LABELS = {
     4: ["N", "E", "S", "W"],
     8: ["N", "NE", "E", "SE", "S", "SW", "W", "NW"],
-    12: [
-        "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"
-    ],
+    12: ["N", "NNE", "ENE", "E", "ESE", "SSE", "S", "SSW", "WSW", "W", "WNW", "NNW"],
 }
 
-def _get_wind_bins_and_labels(n_bins: int):
+
+def _get_wind_bins_and_labels(n_bins: int) -> tuple[np.ndarray, list[str]]:
     if n_bins in WIND_LABELS:
         labels = WIND_LABELS[n_bins]
     else:
@@ -54,14 +53,13 @@ def plot_stacked(
     y_lims: tuple[float, float] = (None, None),
     plot_observation_counts: bool = False,
     sectors_config: dict[str, str] = {},
-    errorbar_kwargs = {
+    errorbar_kwargs={
         "color": "black",
-
         "marker": "x",
         "label": "Measurements",
         "linestyle": "None",
         "alpha": 0.7,
-    }, 
+    },
     wind_bins: int = 8,
 ):
     """Plot stacked bar chart for sectorial fluxes.
@@ -80,7 +78,7 @@ def plot_stacked(
                 * "wind_direction" to group by wind direction sectors
                     This will require a "wind_direction" variable in the dataset.
                     The output will be a wind rose like plot.
-            
+
         species: Name of the species to plot.
         area: Whether to plot the data as an area chart.
         y_lims: Limits for the y-axis.
@@ -161,7 +159,6 @@ def plot_stacked(
 
     fig, ax = plt.subplots(**subplots_kwargs)
 
-
     if "sector_ordering" in sectors_config:
         sector_order = [
             # Set to presciribed order
@@ -182,17 +179,18 @@ def plot_stacked(
         df_sim = pd.concat([df_sim, df_sim.iloc[[0]]])
         df_sim.index = np.append(old_index, 2 * np.pi + old_index[0])
 
-
     ax = stack_plot(
         df_sim,
         ax=ax,
         area=area,
         colors_of_category=sectors_config.get("colors_of_sector", {}),
+        width=0.8 if not wind_plot else (wind_bins[1] - wind_bins[0]),
     )
+    if "yerr" not in errorbar_kwargs:
+        errorbar_kwargs["yerr"] = df_obs["std"].values.reshape(-1)
     ax.errorbar(
         df_obs.index,
         df_obs["mean"].values.reshape(-1),
-        yerr=df_obs["std"].values.reshape(-1),
         **errorbar_kwargs,
     )
     # scatter the total simulated
@@ -207,6 +205,13 @@ def plot_stacked(
     offset = df_obs["mean"].max() * 0.03
     for _, row in df_obs.iterrows():
         kwargs = {"color": "black", "ha": "left", "va": "bottom"}
+        if wind_plot:
+            # Plot away from the center
+            angle = float(row.name)
+            # Correct the angle for meteorological convention
+            angle = np.pi / 2 - angle
+            kwargs["va"] = "top" if np.sin(angle) < 0 else "bottom"
+            kwargs["ha"] = "right" if np.cos(angle) < 0 else "left"
         ax.text(
             row.name,
             row["mean"] + offset,
@@ -219,8 +224,7 @@ def plot_stacked(
                 row.name,
                 row["mean"].value - offset,
                 f"{int(counts[_])}",
-                ha="center",
-                va="top",
+                **kwargs,
             )
 
     handles, labels = ax.get_legend_handles_labels()
@@ -229,16 +233,15 @@ def plot_stacked(
     handles.append(plt.scatter([], [], color="black", marker="$12$"))
 
     labels.append("Count of valid\ncomparisons")
-    ax.legend(handles, labels, loc="center left", bbox_to_anchor=(1, 0.5))
+    x_offset = 1.0 if not wind_plot else 1.06
+    ax.legend(handles, labels, loc="center left", bbox_to_anchor=(x_offset, 0.5))
 
     if wind_plot:
         y_lims = (0, y_lims[1])  # No negative values in wind rose
     ax.set_ylim(y_lims)
 
-    ax.set_ylabel(f"{species} Flux " " [ µmol m$^{-2}$ s$^{-1}$ ]")
     season_str = season if season else ""
-    ax.set_title(f"Footprint and measured fluxes {season_str} ")
-    # ax.set_xlabel()
+    title = f"Footprint and measured fluxes {season_str} "
     x_labels = {
         "%H": "Hour of the day (UTC)",
         "%m": "Month of the year",
@@ -247,7 +250,12 @@ def plot_stacked(
         "%H_%M": "Hour and minute of the day",
     }
     x_label = "Wind direction" if wind_plot else x_labels[group_format]
-    ax.set_xlabel(x_label)
+    y_label = f"{species} Flux " " [ µmol m$^{-2}$ s$^{-1}$ ]"
+    if not wind_plot:
+        ax.set_ylabel(y_label)
+        ax.set_xlabel(x_label)
+    else:
+        title += f" - {y_label}"
     if group_format == "%Y_%m":
         # Rotate the x labels
         ax.set_xticklabels(ax.get_xticklabels(), rotation=90, ha="right")
@@ -256,6 +264,13 @@ def plot_stacked(
         ax.set_theta_direction(-1)
         ax.set_xticks(wind_bins[:-1])
         ax.set_xticklabels(wind_labels)
+        ax.set_rlabel_position(5)
+        # Keep only the tick before the last one
+        y_labels = ax.get_yticklabels()
+        y_labels = ["" if i != len(y_labels) - 1 else l for i, l in enumerate(y_labels)]
+        ax.set_yticklabels(y_labels)
+
+    ax.set_title(title)
     # Make sure to save all the figure and also waht is around it
     fig.tight_layout()
 
