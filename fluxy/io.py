640f86907c31d2703c5e632f27d536644f6558d4
Stephan Henne <stephan.henne@empa.ch>
2025-11-06 14:06:27 +0000

    Bug and typo fixes

--- fluxy/io.py
+++ fluxy/io.py
@@ -320,14 +320,14 @@ def read_model_output(
 
         # Check if file exists
         if not filepath.is_file():
-            logger.warning(f"Cannot find {file_type.value} file: {filepath}. Will try altnative name.")
             #  alternative filename with _flux ending
             if file_type==DataTypes.FLUX:
+                logger.warning(f"Cannot find {file_type.value} file: {filepath}. Will try alternative name.")
                 filepath = get_filename(
                 m,
                 species,
                 period_str,
-                file_pattern(file_type, alternativ=True),
+                file_pattern(file_type, alternative=True),
                 config_data,
                 data_dir,
                 read_standard_run,
--- fluxy/types.py
+++ fluxy/types.py
@@ -22,13 +22,13 @@ def file_pattern(
     Returns the ending pattern for the given file type.
     Args:
         file_type (DataTypes):
-            Type of file (flux or conentration as defined in class DataTypes)
+            Type of file (flux or concentration as defined in class DataTypes)
         alternative (bool): 
             If true an alternative file ending is used for flux files. 
 
     Returns:
         ds_all (str):
-           filename ending depding on data type.  
+           filename ending depending on data type.  
     """
     if file_type == DataTypes.FLUX:
 
