ea13f6db803e306054492ab988a6b2f7a3eea2a1
Daniela Brito Melo <daniela.britomelo@empa.ch>
2025-12-02 14:49:36 +0100

    Make species_info optional

--- fluxy/operators/select.py
+++ fluxy/operators/select.py
@@ -55,9 +55,10 @@ def slice_flux(
 
     """
     ds_all_sliced = dict()
+    species_info = config_data.get("species_info",{})
 
     if species is not None:
-        species_info = config_data["species_info"][species]
+        species_info = species_info.get(species, None)
 
     if type(start_date) is str:
         start_date = [start_date] * len(ds_all.keys())
