7bd6f421a132705c32ea814c2fa25d0a7c4f0f4a
Lionel42 <lionel.constantin@empa.ch>
2026-03-04 08:13:24 +0100

    rename to mix

--- fluxy/operators/mf.py
+++ fluxy/operators/mf.py
@@ -80,8 +80,7 @@ def compute_mf_difference(
         logger.warning(
             f"Different species found {species_set} between the 2 compared datasets."
         )
-        species = species_set
-        species_set = "mix"
+        species = "mix"
     else:
         species = list(species_set)[0]
 
