1daf4302b22755b61825180e2d17d873874d3eb3
Alice Ramsden <alice.ramsden@metoffice.gov.uk>
2026-04-23 11:57:21 +0100

    fixes and new options for sector level bar charts

--- fluxie/plots/flux_timeseries.py
+++ fluxie/plots/flux_timeseries.py
@@ -533,7 +533,7 @@ def add_sector_barplot(
 
     freq = ds_sector.attrs.get("frequency", "unknown")
 
-    if variable == "inv_data" or freq in ["year", "yearly"]:
+    if variable == "inv_data" or freq in ["year", "yearly", "unknown"]:
         if gaps_between_bars:
             d = 300
         else:
@@ -706,6 +706,7 @@ def add_ylabel(
     species: str,
     unit: str,
     plot_type: str,
+    annex_mode: bool = False,
     **kwargs: str | int,
 ):
     """
@@ -726,25 +727,29 @@ def add_ylabel(
     # sector plot - posterior
     elif "sector_barplot" in plot_type:
 
-        print_country = country_equivalent.get(kwargs["region"], kwargs["region"])
-
+        print_country_species = f"{country_equivalent.get(kwargs['region'], kwargs['region'])} {s_data.get(species, {}).get('species_print', species)}"
+        print_units = f"({unit.replace('2','$_{{2}}$').replace('-1','$^{{-1}}$')})"
         if plot_type.split("-")[1] == "posterior":
-            ax.set_ylabel(
-                f"{kwargs['label']}\n{print_country} {s_data.get(species, {}).get('species_print', species)}"
-                f" ({unit.replace('2','$_{{2}}$').replace('-1','$^{{-1}}$')})"
+            if annex_mode:
+                ax.set_ylabel(f"{print_country_species} {print_units}"
             )
+            else:
+             ax.set_ylabel(f"{kwargs['label']}\n{print_country_species} {print_units}"
+            )   
+            
         elif plot_type.split("-")[1] == "prior":
-            ax.set_ylabel(
-                f"Prior\n{print_country} {s_data.get(species, {}).get('species_print', species)}"
-                f" ({unit.replace('2','$_{{2}}$').replace('-1','$^{{-1}}$')})"
+            if annex_mode:
+                ax.set_ylabel(f"Prior\n {print_country_species} {print_units}"
+            )
+            else:
+                ax.set_ylabel(f"Prior\n {print_country_species} {print_units}"
             )
         elif plot_type.split("-")[1] == "inventory":
-            ax.set_ylabel(
-                f"{kwargs['inventory_filename'].replace('_',' ').replace('inventory','Inventory')} {kwargs['year']}\n{print_country} {s_data.get(species, {}).get('species_print', species)}"
-                f" ({unit.replace('2','$_{{2}}$').replace('-1','$^{{-1}}$')})"
-            )
-
-
+            if annex_mode:
+                ax.set_ylabel(f"{print_country_species} {print_units}")
+            else:
+                ax.set_ylabel(f"{kwargs['inventory_filename'].replace('_',' ').replace('inventory','Inventory')} {print_country_species} {print_units}")
+                
 def add_xlims_and_ticks(
     ax: Axes,
     yearly_freq: bool,
@@ -1222,7 +1227,12 @@ def plot_country_sector_flux_bar(
     sectors: list[str] = ["agriculture", "waste", "energy", "industry"],
     xticks_at_centre: bool = False,
     plot_grid: bool = True,
-    gaps_between_bars: bool = False
+    gaps_between_bars: bool = False,
+    plot_separate: bool = False,
+    plot_combined: bool = True,
+    only_overlapping: bool = False,
+    annex_mode: bool = True,
+    vertical_line: str | None = None
 ) -> Figure | list:
     """
     Stacked bar plot of posterior fluxes, split by sector, for a single region, for a range of models.
@@ -1282,16 +1292,19 @@ def plot_country_sector_flux_bar(
         resample=resample,
         rolling_mean=rolling_mean,
         resample_uncert_correlation=resample_uncert_correlation,
+        plot_separate=plot_separate,
+        plot_combined=plot_combined,
+        only_overlapping=only_overlapping
     )
 
-    freqs = [ds.attrs["frequency"] for ds in ds_to_plot.values()]
+    freqs = [ds.attrs["frequency"]  if "frequency" in ds.attrs else "unknown" for ds in ds_to_plot.values()]
 
     if plot_inventory_or_prior == "inventory":
         start_date = str(min([ds.time.values.min() for ds in ds_to_plot.values()]))[:10]
         end_date = str(max([ds.time.values.max() for ds in ds_to_plot.values()]))[:10]
         inv_plot_data = prepare_inventory_sector_barplot(
             sectors,
-            start_date,
+            np.datetime64(start_date,'Y'),
             end_date,
             data_dir,
             plot_region,
@@ -1356,9 +1369,10 @@ def plot_country_sector_flux_bar(
             plot_type=f"{plot_type}-posterior",
             region=plot_region,
             label=ds.attrs["model_label"],
+            annex_mode=annex_mode
         )
         if plot_inventory_or_prior == "prior":
-            ax_comp.legend(ncol=2, borderpad=0.4, columnspacing=1.0)
+            ax_comp.legend(ncol=1, borderpad=0.4, columnspacing=1.0)
             add_ylabel(
                 ax_comp,
                 s_data,
@@ -1366,6 +1380,7 @@ def plot_country_sector_flux_bar(
                 unit,
                 plot_type=f"{plot_type}-prior",
                 region=plot_region,
+                annex_mode=annex_mode
             )
 
     # plot inventory sector bar
@@ -1388,7 +1403,7 @@ def plot_country_sector_flux_bar(
                                          bottom_values, gaps_between_bars)
                 plotted_data_df = pd.concat([plotted_data_df, res], ignore_index=True)
                 former_sector = sector
-            ax.legend(ncol=2, borderpad=0.4, columnspacing=1.0)
+            ax.legend(ncol=1, borderpad=0.4, columnspacing=1.0,loc='upper right')
             add_ylabel(
                 ax,
                 s_data,
@@ -1398,13 +1413,20 @@ def plot_country_sector_flux_bar(
                 region=plot_region,
                 inventory_filename=inventory_filename,
                 year=year,
+                annex_mode=annex_mode
             )
+            
+    # add shading based on vertical line
+    #if vertical_line:
+    #    print(ax_data.get_ylim())
+    #    ax_data.fill_between(ax_data.get_ylim()[0],(np.datetime64(vertical_line),ax_data.get_ylim()[0],ax_data.get_ylim()[1],
+    #                         color='dimgrey',alpha=0.2)
 
     # plot grid and legend
     for ax in axes:
         if plot_grid:
             ax.grid(visible=True, which="major", alpha=0.4)
-        ax_data.legend(ncol=2, borderpad=0.4, columnspacing=1.0)
+        ax_data.legend(ncol=1, borderpad=0.4, columnspacing=1.0,loc='upper right')
 
     # set y lim
     if not fix_y_axes:
