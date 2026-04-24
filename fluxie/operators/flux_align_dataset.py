2a01e57aaa86530657a5223e572d41fe02d16cd8
Valentin Bruch (DWD) <valentin.bruch@dwd.de>
2026-04-24 09:33:21 +0200

    fix coordinate alignment for approximately equal coordinates

--- fluxie/operators/flux_align_dataset.py
+++ fluxie/operators/flux_align_dataset.py
@@ -181,22 +181,23 @@ def align_lat_lon(
             )
 
     # Check if the coordinates agree approximately within the given tolerance.
-    reference = ds_list[0][coord].values
+    ds_ref = ds_list[0]
+    reference = ds_ref[coord].values
     dim_close = all(
         np.allclose(reference, x[coord].values, atol=tolerance) for x in ds_list[1:]
     )
     if dim_close:
         # Use coordinates of first model as reference.
-        aligned_ds_list = [ds_list[0]]
+        aligned_ds_list = [ds_ref]
 
         for ds_p in ds_list[1:]:
-            if ds_list[0][coord].equals(ds_p[coord]):
+            if ds_ref[coord].equals(ds_p[coord]):
                 aligned_ds_list.append(ds_p)
                 continue
             # Replace coordinate with coordinate of first model.
-            ds_aligned = ds_p
-            ds_aligned[coord] = ds_list[0][coord]
-            aligned_ds_list.append(ds_p)
+            ds_aligned = ds_p.copy(deep=False)
+            ds_aligned[coord] = ds_ref[coord]
+            aligned_ds_list.append(ds_aligned)
     else:
         raise ValueError(
             f"{coord} dimensions seem to be too different between the datasets for them to be combined."
