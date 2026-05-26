a3c79a7c7bae67fc8c5f4368cb0bddf05df63490
Alice Ramsden <alice.ramsden@metoffice.gov.uk>
2026-05-26 11:27:02 +0100

    more h2 testing fixes

--- fluxie/config.py
+++ fluxie/config.py
@@ -6,7 +6,7 @@ import os
 logger = logging.getLogger(__name__)
 
 color_palette = {
-    0: [["blue", "dodgerblue"], ["dodgerblue", "skyblue"], ["deepskyblue", "cyan"]],
+    0: [["dodgerblue", "mediumturquoise"], ["firebrick", "darkorange"], ["deepskyblue", "cyan"]],
     1: [["purple", "mediumpurple"], ["deeppink", "pink"], ["darkorange", "red"]],
     2: [["darkgreen", "green"], ["limegreen", "palegreen"], ["olive", "lightgreen"]],
     3: [["darkorange", "orange"], ["gold", "khaki"], ["yellow", "lightyellow"]],
